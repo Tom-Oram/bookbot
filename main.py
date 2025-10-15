a26f3ce60838a33f7d44be5f3191980eeeda2cee
Tom-Oram <e8martint@gmail.com>
2025-10-15 21:57:12 +0100

    added sys.argv to allow multiple inputs

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from stats import get_word_count
 from stats import get_character_count
 from stats import sorted_list
+import sys
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
+def validate_args(expected_count):
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    validate_args(sys.argv)
+    book_text = get_book_text(sys.argv[1])
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     word_count = get_word_count(book_text)
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
